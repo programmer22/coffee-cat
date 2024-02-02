3cb70b522318796f1ecdfae3c5cbc460f76f3cc9
Nick Lopacki <nicklopacki95@gmail.com>
2024-02-02 13:09:22 -0600

    Removed useState from checkout

--- src/components/CheckoutItem.js
+++ src/components/CheckoutItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { quantityHandler, removeFromCart } from '../helpers/cartFunctions'
 
 function CheckoutItem({cartItemData, setCartUpdated}) {
